62ca8c960ea38d326aebc188717150db0b6154d3
Ben Pohl <pohl989@gmail.com>
2018-03-19 10:46:36 -0600

    update lesson

--- 7-Future-ES2015/script.js
+++ 7-Future-ES2015/script.js
@@ -129,10 +129,53 @@
 // Lecture: Arrow functions 2
 
 // ES5
-var box5 = {
-    color: 'green',
-    position: 1,
-    clickMe: function() {
-        
-    }
+// var box5 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: function() {
+//         var self = this;
+//         document.querySelector('.green').addEventListener('click', function(){
+//             var str = 'This box is a number ' + self.position + ' and it is ' + self.color;
+//             alert(str);
+//         });
+//     }
+// }
+// box5.clickMe();
+
+// // ES6 BREAKS THIS and points to global
+// const box66 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: () => {
+//         document.querySelector('.green').addEventListener('click', () => {
+//             var str = `This box is a number ${this.position} and it is ${this.color}`;
+//             alert(str);
+//         });
+//     }
+// }
+// box66.clickMe();
+
+function Person(name) {
+    this.name = name;
 }
+
+
+// //ES5
+// Person.prototype.myFriends5 = function(friends) {
+//     var arr = friends.map(function(el) {
+//         return this.name + ' is friends with ' + el;
+//     }.bind(this));
+//     console.log(arr);
+// }
+//
+// var friends = ['Bob', 'Jane', 'Mark'];
+// new Person('John').myFriends5(friends);
+
+//ES6
+Person.prototype.myFriends6 = function(friends) {
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends6(friends);
